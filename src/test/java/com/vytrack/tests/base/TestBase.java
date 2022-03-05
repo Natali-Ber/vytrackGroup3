dd3f7a36267fb212ac6908af0e83ccdbc1605a29
CrazyDevo <adam@cybertekschool.com>
2022-03-05 11:10:07 -0500

    TestBase.java tearDown method added

--- src/test/java/com/vytrack/tests/base/TestBase.java
+++ src/test/java/com/vytrack/tests/base/TestBase.java
@@ -2,6 +2,7 @@ package com.vytrack.tests.base;
 
 import com.vytrack.utilities.ConfigurationReader;
 import com.vytrack.utilities.Driver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -11,5 +12,9 @@ public class TestBase {
         Driver.getDriver().get(ConfigurationReader.getProperty("env1"));
     }
 
+    @AfterMethod
+    public void tearDown() {
+        Driver.closeDriver();
+    }
 
 }
