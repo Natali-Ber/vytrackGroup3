126819254ae9842a1c3db8bca8dec4f7ba7b1ff6
CrazyDevo <adam@cybertekschool.com>
2022-03-05 12:26:08 -0500

    US1_Test_Main_Modules.java is done

--- src/test/java/com/vytrack/tests/US1_Test_Main_Modules.java
+++ src/test/java/com/vytrack/tests/US1_Test_Main_Modules.java
@@ -1,14 +1,43 @@
 package com.vytrack.tests;
 
 import com.vytrack.tests.base.TestBase;
+import com.vytrack.utilities.Driver;
 import com.vytrack.utilities.VytrackUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class US1_Test_Main_Modules extends TestBase {
 
     @Test
     public void verifying_modules_as_sales_manager(){
         VytrackUtils.loginAsSalesManager();
+        List<WebElement> moduleElements = Driver.getDriver().findElements(By.xpath("//span[@class='title title-level-1']"));
+   List<String> actualmoduleTexts=new ArrayList<>();
+
+        for (WebElement moduleElement : moduleElements) {
+            String moduleElementText = moduleElement.getText();
+            actualmoduleTexts.add(moduleElementText);
+        }
+
+      //  moduleElements.forEach(w->actualmoduleTexts.add(w.getText()));
+        System.out.println("actualmoduleTexts = " + actualmoduleTexts);
+        List<String> expectedmoduleTexts=new ArrayList<>(Arrays.asList(
+                "Dashboards",
+                "Fleet",
+                "Customers",
+                "Sales",
+                "Activities",
+                "Marketing",
+                "Reports & Segments",
+                "System"));
+
+        Assert.assertEquals(actualmoduleTexts, expectedmoduleTexts);
 
     }
 
