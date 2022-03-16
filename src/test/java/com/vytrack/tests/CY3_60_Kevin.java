df7fe44da46ed324008e1319a5c921cd2b887b97
kevinnava <kevin.nava00@gmail.com>
2022-03-16 14:20:41 -0500

    Final Commit

--- src/test/java/com/vytrack/tests/CY3_60_Kevin.java
+++ src/test/java/com/vytrack/tests/CY3_60_Kevin.java
@@ -3,7 +3,6 @@ package com.vytrack.tests;
 import com.vytrack.tests.base.TestBase;
 import com.vytrack.utilities.Driver;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
@@ -36,25 +35,30 @@ public class CY3_60_Kevin extends TestBase {
         Actions actions = new Actions(Driver.getDriver());
         actions.moveToElement(hoverMouse);
 
-        Thread.sleep(4000);
+        Thread.sleep(3000);
 
         //6. Presses on Vehicle option
         WebElement vehicle = Driver.getDriver().findElement(By.xpath("(//span[@class='title title-level-2'])[3]"));
         actions.moveToElement(vehicle);
         actions.click().build().perform();
 
-        Thread.sleep(5000);
+        Thread.sleep(3000);
 
-        //7. Hovers over hamburger icon and displays icons
+        // 7. Hovers over hamburger icon and displays icons
+        WebElement hamburger = Driver.getDriver().findElement(By.xpath("(//a[@class='dropdown-toggle'])[4]"));
+        hamburger.click();
+        hamburger.click();
 
+        Thread.sleep(2000);
 
+        WebElement threeIconPopUp = Driver.getDriver().findElement(By.xpath("(//a[@title='Edit'])[1]"));
 
+        if(threeIconPopUp.isDisplayed()){
+            System.out.println("Passed Icon is displayed!!!!");
+        }else{
+            System.out.println("Failed Icon is not Displayed");
+        }
 
-
-
-
-
-       // (//span[@class='title title-level-2'])[3]
 
     }
 
